053d5af3349656689373debd94d401530fd6347f
manhir <53478716+manhir@users.noreply.github.com>
2020-12-24 15:33:55 +0300

    fix logo console warning

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,29 +1,16 @@
 import s from './styles.module.css'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
 
 export type LogoProps = {
     style?: React.CSSProperties
 }
 
 export const Logo: React.SFC<LogoProps> = props => {
-    const router = useRouter()
-    const main = router.asPath === '/'
-
-    if (main) {
-        return (
-
-            <span className={s.logo} style={props.style}>
-                Набережная В. Тура
-            </span>
-        )
-    }
+    // const router = useRouter()
+    // const main = router.asPath === '/'
 
     return (
-        <Link href={'/'}>
-            <a className={s.logo} style={props.style}>
-                Набережная В. Тура
-            </a>
-        </Link>
+        <span className={s.logo} style={props.style}>
+            Набережная В. Тура
+        </span>
     )
 }
